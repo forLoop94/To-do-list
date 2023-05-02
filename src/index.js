e34cd7061f3f402084ad39f3c1ac1cf41707bec2
forLoop94 <charlesuwem94@gmail.com>
2023-05-02 13:49:56 +0100

    Create an array of tasks(objects) and display them on page load

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './styles.css';
 
+const placeholder = document.querySelector('ul');
+
+
+const simpleTodoTasks = [
+  {
+    description: '',
+    completed: true,
+    index: 0
+  },
+  {
+    description: '',
+    completed: true,
+    index: 0
+  },
+  {
+    description: '',
+    completed: true,
+    index: 0
+  },
+  {
+    description: '',
+    completed: true,
+    index: 0
+  }
+];
+
+const populateEachTask = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const taskDetails = arr[i];
+    const taskContainer = document.createElement('li');
+    taskContainer.className = 'task-container';
+    taskContainer.innerHTML = `<span class='task-content'>${taskDetails.description}</span><span class='task-content'>${taskDetails.completed}</span><span class='task-content'>index:${i}</span>`;
+    placeholder.appendChild(taskContainer);
+  }
+  return placeholder;
+}
+
+populateEachTask(simpleTodoTasks);
+
 
