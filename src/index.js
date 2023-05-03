7325cd4f228fc5baa38eea6741f0524febc49503
Yordi <yorditjordin@gmail.com>
2023-05-03 21:21:09 +0300

    fork try outs

--- src/index.js
+++ src/index.js
@@ -7,28 +7,29 @@ import editTask from './editTask';
 
 const placeholder = document.querySelector('ul');
 
-let simpleTodoTasks = [];
+let simpleTodoTasks = JSON.parse(localStorage.getItem('task')) || [];
 
-const task = localStorage.getItem('task');
-const allTasks = JSON.parse(task);
-if (task) {
-  simpleTodoTasks = allTasks;
-}
+// const task = localStorage.getItem('task');
+// const allTasks = JSON.parse(task);
+// if (task) {
+//   simpleTodoTasks = allTasks;
+// }
 
 form.addEventListener('submit', () => {
   addNewTask(simpleTodoTasks, input.value);
   populateEachTask(simpleTodoTasks);
-  console.log(simpleTodoTasks)
 })
 
 const populateEachTask = (arr) => {
+  localStorage.setItem('task',JSON.stringify(simpleTodoTasks))
   placeholder.innerHTML = '';
   for (let i = 0; i < arr.length; i += 1) {
     const taskDetails = arr[i];
     const taskContainer = document.createElement('li');
+    taskContainer.setAttribute('data-id', i)
     taskContainer.className = 'task-container';
     taskContainer.innerHTML = `<span class='task-content'><input type='checkbox'></span><span class='task-content description'>${taskDetails.item}</span>
-    <span class='edit hide'>${taskDetails.item}</span>
+    <input class='edit hide' value=${taskDetails.item}>
     <span class='task-content index'><i class='fa fa-ellipsis-v'></i></span>`;
 
     const taskDescription = document.querySelectorAll('.description');
@@ -47,22 +48,49 @@ const populateEachTask = (arr) => {
     })
     taskContainer.appendChild(removeBtn);
     placeholder.appendChild(taskContainer);
+
+    editBtn.addEventListener('click', editDecription)
+// loop end
   }
+
 };
 
-editBtn.addEventListener('click', () => {
-  for (let j = 0; j <= taskDescription.length; j += 1) {
-    console.log('cliked j')
-    if (j === i) {
-      console.log('cliked j')
-      // taskDescription.contentEditable = true;
-      // taskDescription.style.backgroundColor = 'red'
-    }
-  }
-  // editTask(simpleTodoTasks, taskDescription);
-  // populateEachTask(simpleTodoTasks);
+const editDecription = (e) => {
+const li = e.target.closest('li')
+const id = li.getAttribute('data-id')
+const description = li.children[1]
+const input = li.children[2]
+//this is edit description
+description.style.display = 'none'
+//input.classList.remove('hide')
+input.classList.toggle('hide', false) //same as removing
+input.focus();
+
+input.addEventListener('keyup', (e)=>{
+  if(e.key === 'Enter'){
+    simpleTodoTasks[id].item = input.value;
+    populateEachTask(simpleTodoTasks)  
+  } 
 })
 
+//console.log('description & input', li, description, input );
+
+  // for (let j = 0; j <= taskDescription.length; j += 1) {
+  //   console.log('cliked i',i,'j',j)
+  //   if (j === i) {
+  //     // taskDescription.contentEditable = true;
+  //     // taskDescription.style.backgroundColor = 'red'
+  //   }
+  // }
+  // // editTask(simpleTodoTasks, taskDescription);
+  // // populateEachTask(simpleTodoTasks);
+}
+
 populateEachTask(simpleTodoTasks);
+
+// const edits = document.querySelectorAll('.fa-pencil')
+// console.log('editsss -------',edits)
+
+
 
 export default simpleTodoTasks;
