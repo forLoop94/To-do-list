af34b71896c70a1e3c1b0edca9c0cd6e818e8086
forLoop94 <charlesuwem94@gmail.com>
2023-05-12 08:39:30 +0100

    Add tests for edit and update functions

---
+++ src/edit_update_clear.test.js
@@ -0,0 +1,29 @@
+import { editTask, updateTaskStatus, clearTasks } from '../_mocks_/edit_update_clear.js';
+
+describe('This function edits, updates and clears all task upon user actions ', () => {
+  test('edit the task', () => {
+    const editItem = editTask({
+      id: 1,
+      task: 'clean the car',
+      status: false,
+    });
+    expect(editItem).toBe(editItem);
+  });
+
+  test('edit the task', () => {
+    const editItem = editTask({
+      id: 1,
+      task: 'write tests for todo list app',
+      status: false,
+    });
+    expect(editItem).toBe(editItem);
+  });
+
+  test('update task status', () => {
+    expect(updateTaskStatus(1)).toStrictEqual({
+      id: 1,
+      task: 'write tests for todo list app',
+      status: true,
+    });
+  });
+});
